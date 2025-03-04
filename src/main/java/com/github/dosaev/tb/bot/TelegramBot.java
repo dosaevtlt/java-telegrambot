5c0883215342b858083b288b98c235ed991b9534
e.dosaev <e.dosaev@kt-team.de>
2025-03-04 13:49:39 +0400

    TB-3: added Command pattern for handling Telegram Bot commands

--- src/main/java/com/github/dosaev/tb/bot/TelegramBot.java
+++ src/main/java/com/github/dosaev/tb/bot/TelegramBot.java
@@ -1,14 +1,18 @@
 package com.github.dosaev.tb.bot;
 
+import com.github.dosaev.tb.command.CommandContainer;
+import com.github.dosaev.tb.service.SendBotMessageServiceImpl;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.springframework.beans.factory.annotation.Value;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import static com.github.dosaev.tb.command.CommandName.NO;
 
 @Component
 public class TelegramBot extends TelegramLongPollingBot {
+
+    public static String COMMAND_PREFIX = "/";
 
     @Value("${bot.username}")
     private String username;
@@ -16,28 +20,30 @@ public class TelegramBot extends TelegramLongPollingBot {
     @Value("${bot.token}")
     private String token;
 
+    private final CommandContainer commandContainer;
+
+    public TelegramBot() {
+        this.commandContainer = new CommandContainer(new SendBotMessageServiceImpl(this));;
+    }
+
     @Override
     public void onUpdateReceived(Update update) {
         if(update.hasMessage() && update.getMessage().hasText()) {
             String message = update.getMessage().getText().trim();
-            String chatId = update.getMessage().getChatId().toString();
 
-            SendMessage sm = new SendMessage();
-            sm.setChatId(chatId);
-            sm.setText(message);
+            if (message.startsWith(COMMAND_PREFIX)) {
+                String commandIdentifier = message.split(" ")[0].toLowerCase();
 
-            try {
-                execute(sm);
-            } catch (TelegramApiException e) {
-                //todo add logging to the project.
-                //e.printStackTrace();
+                commandContainer.retrieveCommand(commandIdentifier).execute(update);
+            } else {
+                commandContainer.retrieveCommand(NO.getCommandName()).execute(update);
             }
         }
     }
 
     @Override
     public String getBotUsername() {
-        return "";
+        return username;
     }
 
     @Override
