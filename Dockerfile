332aef39b7e909171c8dad8db72dffa4f6ac2e73
e.dosaev <e.dosaev@kt-team.de>
2025-03-05 14:23:17 +0400

    TB-13: getting rid of secret data in GitHub repo

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,4 @@
 FROM openjdk:17-jdk-slim
 ARG JAR_FILE=target/*.jar
-ENV BOT_NAME=test_pet_telegram_bot
-ENV BOT_TOKEN=7718978829:AAHhjOVJh2KZCqwZ89b7_dog5-9GXojxaJQ
 COPY ${JAR_FILE} app.jar
-ENTRYPOINT ["java", "-Dbot.username=${BOT_NAME}", "-Dbot.token=${BOT_TOKEN}", "-jar", "/app.jar"]
+ENTRYPOINT ["java", "-jar", "/app.jar"]
--- start.sh
+++ start.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Add environment variables
+export BOT_NAME=$1
+export BOT_TOKEN=$2
+
 # Pull new changes
 git pull
 
@@ -9,10 +13,6 @@ mvn package
 
 # Ensure, that docker-compose stopped
 docker-compose stop
-
-# Add environment variables
-export BOT_NAME=$1
-export BOT_TOKEN=$2
 
 # Start new deployment
 docker-compose up --build -d
