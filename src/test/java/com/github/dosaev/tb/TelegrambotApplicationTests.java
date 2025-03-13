6e4c613d99ea58685ceeb3e4f557ad4539f7ab6f
e.dosaev <e.dosaev@kt-team.de>
2025-03-13 14:33:18 +0400

    TB-1: for MYSQL in CI needed own creds

--- src/test/java/com/github/dosaev/tb/TelegrambotApplicationTests.java
+++ src/test/java/com/github/dosaev/tb/TelegrambotApplicationTests.java
@@ -8,10 +8,10 @@ import org.springframework.test.context.ActiveProfiles;
 @SpringBootTest
 class TelegrambotApplicationTests {
 
-	static {
+	/*static {
 		System.setProperty("BOT_NAME", "test_pet_telegram_bot");
 		System.setProperty("BOT_TOKEN", "7718978829:AAHhjOVJh2KZCqwZ89b7_dog5-9GXojxaJQ");
-	}
+	}*/
 
 	@Test
 	void contextLoads() {
